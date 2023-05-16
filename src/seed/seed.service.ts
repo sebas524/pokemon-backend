58ddb7a09ab9cb2c8262e88ff1c1e6181795a974
sebas524 <104307242+sebas524@users.noreply.github.com>
2023-05-16 10:33:17 +0200

    after inserting by lot

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
 
   async excecuteSeed() {
     const { data } = await this.axios.get<PokeResponse>(
@@ -13,14 +21,15 @@ export class SeedService {
 
     // * now we need the name and id. name is easy cause its in results[0]. but id is inside of url of specific pokemon. therefore:
 
-    data.results.forEach(({ name, url }) => {
+    data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/');
       // * its -2 because its the second to last index
-      const pokemonNum = +segments[segments.length - 2];
+      const num = +segments[segments.length - 2];
       // * after youve done this, then you have:
-      console.log({ name, pokemonNum });
+      console.log({ name, num });
+      const pokemon = await this.pokemonModel.create({ name, num });
     });
 
-    return data.results;
+    return 'seed executed';
   }
 }
